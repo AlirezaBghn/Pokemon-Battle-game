ce771ff2762f48cea20c7839ca4a957a3e9a2745
Alireza Baghban <alireza.baghbann@gmail.com>
2025-03-14 14:55:58 +0100

    server.js Updated
    allowedHeaders and methods added

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,13 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.use(express.json());
 app.use(cookieParser());
