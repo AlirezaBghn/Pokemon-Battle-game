ece6be9c181fcccf5b55fe77d525f3d241a6b903
Alireza Baghban <alireza.baghbann@gmail.com>
2025-03-14 13:51:11 +0100

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,8 +4,7 @@ const connectDB = async () => {
   try {
     console.log("��� Connecting to MongoDB...");
     await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      
     });
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
