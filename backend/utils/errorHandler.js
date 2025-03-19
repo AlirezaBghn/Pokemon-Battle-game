311c1e0f1067542e61343ede722a838c07da4165
Alireza Baghban <alireza.baghbann@gmail.com>
2025-03-20 00:31:30 +0100

    Update errorHandler.js

--- backend/utils/errorHandler.js
+++ backend/utils/errorHandler.js
@@ -1,9 +1,3 @@
-const notFound = (req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-};
-
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
@@ -14,4 +8,4 @@ const errorHandler = (err, req, res, next) => {
   });
 };
 
-export { notFound, errorHandler };
+export { errorHandler };
